70abdb0a96886ac53b59eeb5e5b8aea6021dd2ef
cokvrindaa <master.cokorda@gmail.com>
2025-11-12 08:50:49 +0800

    function

--- js/FUNCTION/basicFunction.js
+++ js/FUNCTION/basicFunction.js
@@ -6,3 +6,19 @@ function ferenhitkeCelcius(suhu = 50) {
 const input = 90;
 ferenhitkeCelcius();
 ferenhitkeCelcius(input);
+
+// Penggunaan return
+function tambah(a, b){
+  const hasil = a + b;
+  return hasil;
+}
+const hasil = tambah(2,2); //4
+console.log(hasil);
+
+// improvisasi kode menggunakan return
+function ferenhitkeCelciusBaru(suhu) {
+  const rumus = (9 / 5) * suhu + 32;
+  return rumus;
+}
+const hasilbaru = ferenhitkeCelciusBaru(90); //194
+console.log(hasilbaru);
